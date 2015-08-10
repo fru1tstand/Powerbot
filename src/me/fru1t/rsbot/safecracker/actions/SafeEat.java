ca08c0f050fa99180348fa3e4685b34ca8f3875e
Kodlee Yin <kodlee@kodleeshare.net>
2015-08-09 22:56:30 -0400

    Finished refactor delete of Persona in favor of adding the logic to Action modules

--- src/me/fru1t/rsbot/safecracker/actions/SafeEat.java
+++ src/me/fru1t/rsbot/safecracker/actions/SafeEat.java
@@ -3,32 +3,47 @@ package me.fru1t.rsbot.safecracker.actions;
 import org.powerbot.script.rt6.ClientContext;
 import org.powerbot.script.rt6.Item;
 
+import me.fru1t.annotations.Inject;
 import me.fru1t.rsbot.RoguesDenSafeCracker;
 import me.fru1t.rsbot.framework.Action;
-import me.fru1t.rsbot.safecracker.Persona;
+import me.fru1t.rsbot.framework.components.RunState;
 import me.fru1t.rsbot.safecracker.Settings;
+import me.fru1t.rsbot.safecracker.actions.safeeat.FoodLogic;
 
-public class SafeEat extends Action<ClientContext, RoguesDenSafeCracker, Settings, Persona> {
-	public SafeEat(RoguesDenSafeCracker script) {
-		super(script);
+public class SafeEat implements Action {
+	private final ClientContext ctx;
+	private final RunState<RoguesDenSafeCracker.State> state;
+	private final Settings settings;
+	private final FoodLogic foodLogic;
+	
+	@Inject
+	public SafeEat(
+			ClientContext ctx,
+			RunState<RoguesDenSafeCracker.State> state,
+			Settings settings,
+			FoodLogic foodLogic) {
+		this.ctx = ctx;
+		this.state = state;
+		this.settings = settings;
+		this.foodLogic = foodLogic;
 	}
 
 	@Override
 	public boolean run() {
 		// Out of food?
-		if (script.ctx.backpack.select().id(script.settings.getCurrentFood().id).isEmpty()) {
-			script.updateState(RoguesDenSafeCracker.State.BANK_WALK);
+		if (ctx.backpack.select().id(settings.getCurrentFood().id).isEmpty()) {
+			state.update(RoguesDenSafeCracker.State.BANK_WALK);
 			return true;
 		}
 		
-		int eatCount = script.persona.foodToConsume();
-		while (!script.ctx.backpack.select().id(script.settings.getCurrentFood().id).isEmpty()
-				&& eatCount > 0) {
-			Item food = script.ctx.backpack.poll();
+		int eatCount = foodLogic.numberToEat();
+		while (!ctx.backpack.select().id(settings.getCurrentFood().id).isEmpty()
+				&& eatCount-- > 0) {
+			Item food = ctx.backpack.poll();
 			if (!food.inViewport()) {
-				script.ctx.backpack.scroll(food);
+				ctx.backpack.scroll(food);
 			}
-			
+			food.click();
 		}
 		return true;
 	}
---
+++ src/me/fru1t/rsbot/safecracker/actions/safeeat/FoodLogic.java
@@ -0,0 +1,62 @@
+package me.fru1t.rsbot.safecracker.actions.safeeat;
+
+import org.powerbot.script.rt6.ClientContext;
+
+import me.fru1t.annotations.Inject;
+import me.fru1t.rsbot.safecracker.Settings;
+import me.fru1t.rsbot.utils.Random;
+
+/**
+ * There are people who eat one food, those who eat multiple food, and those who eat until the HP
+ * is full.
+ * 
+ * <p>TODO: Add overeat
+ */
+public class FoodLogic {
+	// Each eat stype is equally as likely to be chosen.
+	private enum EatStyle { ONE, MULTIPLE, FULL }
+	
+	private final ClientContext ctx;
+	private final Settings settings;
+	private final EatStyle eatStyle;
+	
+	@Inject
+	public FoodLogic(ClientContext ctx, Settings settings) {
+		this.ctx = ctx;
+		this.settings = settings;
+		
+		int rnd = Random.nextInt(0, 100);
+		if (rnd < 33) {
+			eatStyle = EatStyle.ONE;
+		} else if (rnd < 66) {
+			eatStyle = EatStyle.MULTIPLE;
+		} else {
+			eatStyle = EatStyle.FULL;
+		}
+	}
+	
+	/**
+	 * @return The number of food items to eat.
+	 */
+	public int numberToEat() {
+		switch(eatStyle) {
+		case FULL:
+			return getPossibleFoodConsumptionAmount();
+		case MULTIPLE:
+			// TODO: Ehh...?
+			return Random.nextInt(1, getPossibleFoodConsumptionAmount() + 1);
+		case ONE:
+		default:
+			return 1;
+		}
+	}
+	
+	/**
+	 * @return The number of food one can eat without overhealing.
+	 */
+	private int getPossibleFoodConsumptionAmount() {
+		// TODO: Is this the correct way to get both EOC and legacy hp?
+		return (ctx.combatBar.maximumHealth() - ctx.combatBar.health())
+				/ settings.getCurrentFood().healAmount;
+	}
+}

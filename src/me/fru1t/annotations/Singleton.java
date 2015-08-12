dd95be3a8d9ab258d45c7054a93403050fd9ce5e
Kodlee Yin <kodlee@kodleeshare.net>
2015-08-11 21:40:16 -0400

    Added @Singleton annotation for future use

---
+++ src/me/fru1t/annotations/Singleton.java
@@ -0,0 +1,17 @@
+package me.fru1t.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Denotes that the {parameter, type} should only ever be instantiated once per Slick instance.
+ * 
+ * 
+ * <p>Both parameter and type *must* have this annotation, otherwise Slick will throw an exception.
+ * This behavior is a design choice intended to make the slick-program contract more transparent.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+public @interface Singleton { }

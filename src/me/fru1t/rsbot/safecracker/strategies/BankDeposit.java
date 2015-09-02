5b2e98721b071752d2281f5e6ac2969ef076fd5f
Kodlee Yin <kodlee@kodleeshare.net>
2015-09-01 23:44:00 -0400

    Finished BankDeposit

--- src/me/fru1t/rsbot/safecracker/strategies/BankDeposit.java
+++ src/me/fru1t/rsbot/safecracker/strategies/BankDeposit.java
@@ -1,22 +1,46 @@
 package me.fru1t.rsbot.safecracker.strategies;
 
+import org.powerbot.script.rt6.ClientContext;
+
 import me.fru1t.common.annotations.Inject;
+import me.fru1t.common.annotations.Singleton;
 import me.fru1t.rsbot.RoguesDenSafeCracker;
 import me.fru1t.rsbot.RoguesDenSafeCracker.State;
 import me.fru1t.rsbot.common.framework.Strategy;
+import me.fru1t.rsbot.common.script.rt6.BankUtil;
+import me.fru1t.rsbot.safecracker.strategies.logic.DepositInventoryButton;
 
+/**
+ * Does the bank depositing.
+ */
 public class BankDeposit implements Strategy<RoguesDenSafeCracker.State> {
-	@Inject
-	public BankDeposit() {
+	private final ClientContext ctx;
+	private final BankUtil bankUtil;
+	private final DepositInventoryButton depositInventoryButton;
 
+	@Inject
+	public BankDeposit(
+			@Singleton ClientContext ctx,
+			@Singleton BankUtil bankUtil,
+			DepositInventoryButton depositInventoryButton) {
+		this.ctx = ctx;
+		this.bankUtil = bankUtil;
+		this.depositInventoryButton = depositInventoryButton;
 	}
 
 	@Override
 	public State run() {
-		// We aren't certain if the bank is open at this time.
-		
-		
-		// TODO Auto-generated method stub
-		return null;
+		// TODO(v2): More fail-safe catch states
+		if (ctx.backpack.isEmpty()) {
+			return State.BANK_WITHDRAW;
+		}
+
+		if (depositInventoryButton.shouldClick()) {
+			if (!bankUtil.clickDepositInventory()) {
+				return State.BANK_OPEN;
+			}
+		}
+
+		return State.BANK_WITHDRAW;
 	}
 }

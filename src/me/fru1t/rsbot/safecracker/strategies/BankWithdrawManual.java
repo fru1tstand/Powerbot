4830f199523e0931b53a99967083610b74b3ae7a
Kodlee Yin <kodlee@kodleeshare.net>
2015-09-05 17:11:14 -0400

    Added manual bank withdraw mode

---
+++ src/me/fru1t/rsbot/safecracker/strategies/BankWithdrawManual.java
@@ -0,0 +1,40 @@
+package me.fru1t.rsbot.safecracker.strategies;
+
+import org.powerbot.script.rt6.ClientContext;
+
+import me.fru1t.common.annotations.Inject;
+import me.fru1t.common.annotations.Singleton;
+import me.fru1t.rsbot.RoguesDenSafeCracker;
+import me.fru1t.rsbot.RoguesDenSafeCracker.State;
+import me.fru1t.rsbot.common.framework.Strategy;
+import me.fru1t.rsbot.common.script.rt6.BankUtil;
+import me.fru1t.rsbot.safecracker.Settings;
+
+public class BankWithdrawManual implements Strategy<RoguesDenSafeCracker.State> {
+	private final ClientContext ctx;
+	private final BankUtil bankUtil;
+	private final Settings settings;
+
+	@Inject
+	public BankWithdrawManual(
+			@Singleton ClientContext ctx,
+			@Singleton BankUtil bankUtil,
+			@Singleton Settings settings) {
+		this.ctx = ctx;
+		this.bankUtil = bankUtil;
+		this.settings = settings;
+	}
+
+	@Override
+	public State run() {
+		if (!bankUtil.waitForBankToOpen()) {
+			return State.BANK_OPEN;
+		}
+
+		if (!ctx.bank.withdraw(settings.getFood().id, settings.getFoodQuantity())) {
+			return null;
+		}
+
+		return State.SAFE_WALK;
+	}
+}

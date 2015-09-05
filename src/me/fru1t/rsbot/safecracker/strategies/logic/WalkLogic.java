c9ebc5144733ec1890c0bdb23da81c26fcf199ba
Kodlee Yin <kodlee@kodleeshare.net>
2015-09-05 18:27:53 -0400

    Added walk logic

--- src/me/fru1t/rsbot/safecracker/strategies/logic/WalkLogic.java
+++ src/me/fru1t/rsbot/safecracker/strategies/logic/WalkLogic.java
@@ -1,5 +1,38 @@
 package me.fru1t.rsbot.safecracker.strategies.logic;
 
+import me.fru1t.common.annotations.Inject;
+import me.fru1t.rsbot.common.util.Probability;
+import me.fru1t.rsbot.common.util.Random;
+
 public class WalkLogic {
-	private static final
+	public enum WalkMethod implements Probability {
+		// The user clicks on the minimap to traverse to the safe location
+		MINIMAP(50),
+
+		// The user clicks on the viewport to traverse to the safe location.
+		// In some instances, will simply skip the traverse all together and interact with the safe
+		// directly.
+		VIEWPORT(50);
+
+		private final int probability;
+		private WalkMethod(int probability) {
+			this.probability = probability;
+		}
+
+		@Override
+		public int getProbability() {
+			return probability;
+		}
+	}
+
+	private final WalkMethod walkMethod;
+
+	@Inject
+	public WalkLogic() {
+		this.walkMethod = Random.roll(WalkMethod.class);
+	}
+
+	public WalkMethod getWalkMethod() {
+		return walkMethod;
+	}
 }

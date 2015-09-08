0e64a23bb2e9a06727f9cd743d8e3714eeca0fd5
Kodlee Yin <kodlee@kodleeshare.net>
2015-09-07 22:58:22 -0700

    Added javadoc to Callables.java

--- src/me/fru1t/rsbot/common/framework/util/Callables.java
+++ src/me/fru1t/rsbot/common/framework/util/Callables.java
@@ -5,6 +5,13 @@ import java.util.concurrent.Callable;
 public final class Callables {
 	private Callables() { }
 
+	/**
+	 * Creates a simple Callable that is of the given parameter's type and that only returns the
+	 * given parameter.
+	 *
+	 * @param response The response that the returning Callable's call method should return.
+	 * @return A Callable object that returns the given response.
+	 */
 	public static <T> Callable<T> of(final T response) {
 		return new Callable<T>() {
 			@Override

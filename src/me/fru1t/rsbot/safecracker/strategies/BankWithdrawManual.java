3c6b7d9f25b076abe0bad8919169bce92b43ff7f
Kodlee Yin <kodlee@kodleeshare.net>
2015-09-13 22:57:24 -0700

    Finalized BankWithdraw strategies

--- src/me/fru1t/rsbot/safecracker/strategies/BankWithdrawManual.java
+++ src/me/fru1t/rsbot/safecracker/strategies/BankWithdrawManual.java
@@ -1,5 +1,6 @@
 package me.fru1t.rsbot.safecracker.strategies;
 
+import me.fru1t.rsbot.common.framework.components.Status;
 import me.fru1t.slick.util.Provider;
 import org.powerbot.script.rt6.ClientContext;
 
@@ -14,21 +15,27 @@ import me.fru1t.rsbot.safecracker.Settings;
 public class BankWithdrawManual implements Strategy<RoguesDenSafeCracker.State> {
 	private final Provider<ClientContext> ctxProvider;
 	private final Provider<Settings> settingsProvider;
+	private final Provider<Status> statusProvider;
 	private final Bank bankUtil;
 
 	@Inject
 	public BankWithdrawManual(
 			Provider<ClientContext> ctxProvider,
 			Provider<Settings> settingsProvider,
+			Provider<Status> statusProvider,
 			@Singleton Bank bankUtil) {
 		this.ctxProvider = ctxProvider;
 		this.settingsProvider = settingsProvider;
+		this.statusProvider = statusProvider;
 		this.bankUtil = bankUtil;
 	}
 
 	@Override
 	public State run() {
+		statusProvider.get().update("Withdrawing items manually");
+
 		if (!bankUtil.waitForBankToOpen()) {
+			statusProvider.get().update("The bank never opened");
 			return State.BANK_OPEN;
 		}
 
--- src/me/fru1t/rsbot/safecracker/strategies/BankWithdrawWithPresets.java
+++ src/me/fru1t/rsbot/safecracker/strategies/BankWithdrawWithPresets.java
@@ -5,6 +5,7 @@ import me.fru1t.common.annotations.Singleton;
 import me.fru1t.rsbot.RoguesDenSafeCracker;
 import me.fru1t.rsbot.RoguesDenSafeCracker.State;
 import me.fru1t.rsbot.common.framework.Strategy;
+import me.fru1t.rsbot.common.framework.components.Status;
 import me.fru1t.rsbot.common.script.rt6.Bank;
 import me.fru1t.rsbot.safecracker.Settings;
 import me.fru1t.rsbot.safecracker.Settings.BankStyle;
@@ -13,20 +14,23 @@ import me.fru1t.slick.util.Provider;
 public class BankWithdrawWithPresets implements Strategy<RoguesDenSafeCracker.State> {
 	private final Bank bankUtil;
 	private final Provider<Settings> settingsProvider;
+	private final Provider<Status> statusProvider;
 
 	@Inject
 	public BankWithdrawWithPresets(
-			@Singleton Bank bankUtil,
-			Provider<Settings> settingsProvider) {
+			Provider<Settings> settingsProvider,
+			Provider<Status> statusProvider,
+			@Singleton Bank bankUtil) {
 		this.bankUtil = bankUtil;
 		this.settingsProvider = settingsProvider;
+		this.statusProvider = statusProvider;
 	}
 
 	@Override
 	public State run() {
-		// TODO(v2): Are there missing prerequisites that may lurk?
-
+		statusProvider.get().update("Withdrawing using preset buttons");
 		// TODO(v1 cleanup): Implement convenience methods to clear up this mess
+
 		if (settingsProvider.get().isBankStyle(BankStyle.PRESET_1)) {
 			if (!bankUtil.clickPreset1()) {
 				return State.BANK_OPEN;
